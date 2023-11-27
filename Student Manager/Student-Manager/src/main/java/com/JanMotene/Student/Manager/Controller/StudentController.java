66b760318dafe8be6e94d185eb74770103258749
Jan Motene <motenejo@gmail.com>
2023-11-27 17:25:09 +0200

    Adding REST API to start on server

--- Student Manager/Student-Manager/src/main/java/com/JanMotene/Student/Manager/Controller/StudentController.java
+++ Student Manager/Student-Manager/src/main/java/com/JanMotene/Student/Manager/Controller/StudentController.java
@@ -3,16 +3,20 @@ package com.JanMotene.Student.Manager.Controller;
 import com.JanMotene.Student.Manager.Model.Student;
 import com.JanMotene.Student.Manager.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/students")
 public class StudentController {
 
     @Autowired
     private StudentRepository studentRepository;
 
+    @GetMapping
     public List<Student> getAllStudent(){
         return studentRepository.findAll();
     }
--- Student Manager/Student-Manager/src/main/java/com/JanMotene/Student/Manager/StudentManagerApplication.java
+++ Student Manager/Student-Manager/src/main/java/com/JanMotene/Student/Manager/StudentManagerApplication.java
@@ -1,13 +1,35 @@
 package com.JanMotene.Student.Manager;
 
+import com.JanMotene.Student.Manager.Model.Student;
+import com.JanMotene.Student.Manager.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagerApplication {
+public class StudentManagerApplication implements CommandLineRunner {
+
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(StudentManagerApplication.class, args);
 	}
 
+	@Autowired
+	private StudentRepository studentRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		Student student = new Student();
+		student.setFirstName("Jan");
+		student.setLastName("Motene");
+		student.setEmailId("motenejo@gmail.com");
+		studentRepository.save(student);
+
+		Student student1 = new Student();
+		student1.setFirstName("Elon");
+		student1.setLastName("Musk");
+		student1.setEmailId("Elon@gmail.com");
+		studentRepository.save(student1);
+	}
 }
